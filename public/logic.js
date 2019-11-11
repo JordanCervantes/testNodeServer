5575e8472a1a9d40249290f79d007c81cbc8adfd
Jordan Cervantes <jordan.cervantes@yahoo.com>
2019-11-11 18:39:39 -0500

    1.0 Completion
    Adding ajax requests and genrerated table for individual line items.

--- public/logic.js
+++ public/logic.js
@@ -3,33 +3,51 @@ $(document).ready(function(){
 });
 $( "#subButn" ).click(function() {
     var inputVal = $('#repoInput').val();
-    console.log(inputVal);
+    //console.log(inputVal);
     var pathArray = inputVal.split('/');
-    console.log(pathArray);
+    //console.log(pathArray);
     var userName = pathArray[3];
     var repoName = pathArray[4];
-    console.log(userName);
-    console.log(repoName);
+    //console.log(userName);
+    //console.log(repoName);
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://localhost:8080/dependencies?repo=" + userName + "&user=" + repoName,
+        "url": "http://localhost:8080/dependencies?repo=" + repoName + "&user=" + userName,
         "method": "GET",
-        "headers": {
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-          "User-Agent": "PostmanRuntime/7.19.0",
-          "Accept": "*/*",
-          "Cache-Control": "no-cache",
-          "Postman-Token": "42fee966-8e35-4860-935d-357d44e90d5c,51280a83-c549-4159-89ec-d97027d9cfd6",
-          "Host": "localhost:8080",
-          "Accept-Encoding": "gzip, deflate",
-          "Connection": "keep-alive",
-          "cache-control": "no-cache"
-        }
       }
-      console.log(settings);
+      //console.log(settings);
       
       $.ajax(settings).done(function (response) {
         console.log(response);
       });
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/dependencies/lifecheck",
+        "dataType":"json",
+        "method": "GET",
+        }
+
+        $.ajax(settings).done(function (response) {
+          console.log(response);
+
+          if(response){
+            var len = response.length;
+            var txt = "";
+            if(len > 0){
+                for(var i=0;i<len;i++){
+                    if(response[i].moduleName && response[i].installed && response[i].latest){
+                        txt += "<tr><td>"+response[i].moduleName+"</td><td>"+response[i].installed+"</td><td>"+response[i].latest+"</td></tr>";
+                    }
+                }
+                if(txt != ""){
+                    $("#table").append(txt).removeClass("hidden");
+                }
+            }
+        }
+    },
+
+        );
   });
+  
